66d1a5658b396cd539fa55a205700f6ed1f3ef45
Gia Huy Hồ <giahuyho@Gias-MacBook-Pro.local>
2023-02-15 01:13:53 +0700

    tang

---
+++ src/pages/tang-em.tsx
@@ -0,0 +1,8 @@
+export default function TangEm() {
+  return (
+    <img
+      src="http://atlas-content-cdn.pixelsquid.com/stock-images/rose-y1JqOq1-600.jpg"
+      className="w-screen h-screen object-contain"
+    />
+  );
+}
